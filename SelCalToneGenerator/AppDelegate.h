d19a83538516a0a405d63fc2db883dc765c59112
Robert Miller <robertmiller@Roberts-MacBook-Pro.local>
2012-06-20 09:55:05 -0400

    Initial Commit

---
+++ SelCalToneGenerator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SelCalToneGenerator
+//
+//  Created by Robert Miller on 6/20/12.
+//  Copyright (c) 2012 Gables Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SelCalToneGenerator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SelCalToneGenerator
+//
+//  Created by Robert Miller on 6/20/12.
+//  Copyright (c) 2012 Gables Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
